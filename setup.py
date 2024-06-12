b46584ba53bd8ac9dc90f9e57806b58b7418eda1
Zeyuli <li_zeyu@pku.edu.cn>
2024-06-12 18:41:42 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='PerfLLM',
+      version='0.1',
+      description='PerfLLM',
+      url='http://github.com/Galesleee/PerfLLM',
+      author='Zeyu Li',
+      author_email='zeyuli0123@icloud.com',
+      license='MIT',
+      packages=['PerfLLM'],
+      zip_safe=False)
